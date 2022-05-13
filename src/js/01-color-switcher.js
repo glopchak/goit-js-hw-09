9773c434a81a1f4fe7b4d1a8ae99d179ddf2126d
Diana Glopchak <dglopchak@gmail.com>
2022-05-13 17:34:04 +0300

    task 1

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,34 @@
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
 
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+
+const timeOut = 1000;
+
+const colorSwitch = {
+  itervalId: null,
+
+  start() {
+    this.itervalId = setInterval(() => {
+      body.style = `background-color: ${getRandomHexColor()}`;
+    }, timeOut);
+
+    btnStart.setAttribute('disabled', true);
+  },
+
+  stop() {
+    clearInterval(this.itervalId);
+
+    btnStart.removeAttribute('disabled');
+  },
+};
+
+btnStart.addEventListener('click', () => {
+  colorSwitch.start();
+});
+btnStop.addEventListener('click', () => {
+  colorSwitch.stop();
+});
